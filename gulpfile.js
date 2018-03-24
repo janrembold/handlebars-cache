c49e445b0c0b96db1a1ffa90b6ccbfbab3079a6d
Jan Rembold <janrembold@googlemail.com>
2018-03-24 22:42:51 +0100

    refactored to handlebars wax usage as PoC

--- gulpfile.js
+++ gulpfile.js
@@ -2,40 +2,30 @@ const gulp = require('gulp');
 const hb = require('gulp-hb');
 const watch = require('gulp-watch');
 const handlebars = require('handlebars');
-const globule = require('globule');
+const handlebarsWax = require('handlebars-wax');
 const fs = require('fs');
-const path = require('path');
-
 
 
 // Initially load and register all partials
-const paths = globule.find([
-    'src/*.hbs',
-    '!src/index.hbs'
-]);
-
-for(let filePath of paths) {
-    const parsedPath = path.parse(filePath);
-    handlebars.registerPartial(parsedPath.name, fs.readFileSync(filePath, 'utf8'));
-}
-
-
+const wax = handlebarsWax(handlebars)
+    .partials([
+        'src/*.hbs',
+        '!src/index.hbs'
+    ]);
 
 gulp.task('hb', () => {
     console.log('### Initial compilation of index.hbs ###');
-    console.log(handlebars.compile(fs.readFileSync('src/index.hbs', 'utf8'))());
+    console.log(wax.compile(fs.readFileSync('src/index.hbs', 'utf8'))());
 });
 
 gulp.task('hb:watch', () => {
     watch('src/*.hbs', {}, (vinyl) => {
 
-        const parsedPath = path.parse(vinyl.path);
-
         // Update partial on-the-fly
-        handlebars.registerPartial(parsedPath.name, fs.readFileSync(vinyl.path, 'utf8'));
+        wax.partials(vinyl.path);
 
-        console.log(`### Updated ${parsedPath.name} from ${vinyl.path} ###`);
-        console.log(handlebars.compile(fs.readFileSync('src/index.hbs', 'utf8'))());
+        console.log(`### Updated ${vinyl.path} ###`);
+        console.log(wax.compile(fs.readFileSync('src/index.hbs', 'utf8'))());
 
     });
 });
