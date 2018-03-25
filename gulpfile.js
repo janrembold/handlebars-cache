7623f7ed43f0abc0e167d993c36daa3eecd1ebcd
Jan Rembold <janrembold@googlemail.com>
2018-03-25 10:42:07 +0200

    refactored to use single handlebars instance and update partial directly inside handlebars

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 const gulp = require('gulp');
 const hb = require('gulp-hb');
+const handlebars = require('handlebars');
 const pump = require('pump');
 const watch = require('gulp-watch');
 const runSequence = require('run-sequence');
 const rename = require('gulp-rename');
+const path = require('path');
+const fs = require('fs');
 
 // Initially load all partials
-const hbStream = hb().partials('src/*.hbs');
+const hbStream = hb({
+    bustCache: true,
+    handlebars: handlebars
+}).partials('src/*.hbs');
 
 
 gulp.task('hb', (cb) => {
@@ -24,8 +30,9 @@ gulp.task('hb', (cb) => {
 gulp.task('hb:watch', () => {
     watch('src/*.hbs', {}, (vinyl) => {
 
-        console.log(`Add partial ${vinyl.path}`);
-        hbStream.partials(vinyl.path);
+        console.log(`Change partial ${vinyl.path}`);
+        const parsedPath = path.parse(vinyl.path);
+        handlebars.registerPartial(parsedPath.name, fs.readFileSync(vinyl.path, 'utf8'));
         runSequence('hb');
 
     });
